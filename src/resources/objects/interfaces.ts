8496c36a7e138223fad58058103119390a8b9001
Chris Bell <cjbell1988@gmail.com>
2023-02-03 14:51:12 -0500

    feat(objects): add subscriptions methods

--- src/resources/objects/interfaces.ts
+++ src/resources/objects/interfaces.ts
@@ -1,4 +1,6 @@
 import { CommonMetadata, PaginationOptions } from "../../common/interfaces";
+import { User } from "../users/interfaces";
+import { Recipient, RecipientWithUpsert } from "../workflows/interfaces";
 
 export interface ObjectRef {
   collection: string;
@@ -27,4 +29,22 @@ export interface BulkSetObjectOption {
 export interface ListObjectOptions extends PaginationOptions {
   object_id?: string;
   name?: string;
+}
+
+export interface ListObjectSubscriptionsOptions extends PaginationOptions {}
+
+export interface ObjectSubscription<T = CommonMetadata> {
+  recipient: User | Object;
+  properties: T;
+  inserted_at: string;
+  updated_at: string;
+}
+
+export interface AddObjectSubscriptionProperties {
+  recipients: (Recipient | RecipientWithUpsert)[];
+  properties?: CommonMetadata;
+}
+
+export interface DeleteObjectSubscriptionProperties {
+  recipients: Recipient[];
 }
