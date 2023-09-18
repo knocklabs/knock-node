d9d6ec80db8e8001133d37ce5875dfe9f5adb15b
Franco Borrelli <franco@mimiquate.com>
2023-09-18 10:19:43 -0300

    feat(KNO-4322): add webhook channel type

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -20,7 +20,7 @@ export interface UnprocessableEntityError {
 
 // Channel types supported in Knock
 // TODO: it would be great to pull this in from an external location
-export type ChannelType = "email" | "in_app_feed" | "sms" | "push" | "chat";
+export type ChannelType = "email" | "in_app_feed" | "sms" | "push" | "chat" | "http";
 
 export type CommonMetadata = Record<string, any>;
 
