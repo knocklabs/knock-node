5853f192c18bc88c9c3ec398b6d2e2252b9660e0
natperpepaj <162606520+natperpepaj@users.noreply.github.com>
2024-04-01 16:34:35 -0400

    chore(kno-5596): add support for objects and recipients filtering (#65)

--- src/common/fetchClient.ts
+++ src/common/fetchClient.ts
@@ -90,6 +90,10 @@ export default class FetchClient {
   private buildUrl(path: string, params?: FetchRequestConfig["params"]): URL {
     const url = new URL(this.config.baseURL + path);
 
+    function containsObject(array: any[]): boolean {
+      return array.some((item) => typeof item === "object" && item !== null);
+    }
+
     function appendParams(key: string, value: any, parentKey?: string) {
       const fullKey = parentKey ? `${parentKey}[${key}]` : key;
 
@@ -102,6 +106,23 @@ export default class FetchClient {
         // If value is an object, recurse
         Object.entries(value).forEach(([nestedKey, nestedValue]) => {
           appendParams(nestedKey, nestedValue, fullKey);
+        });
+      } else if (Array.isArray(value) && containsObject(value)) {
+        // Send array values as individual values with identifier
+        // e.g. key[0]=user1&key[1]=userid2&key[3][field1Key]=field1Value&key[3][field2Key]=field2Value
+        value.forEach((item, index) => {
+          const indexKey = index.toString();
+          if (typeof item === "string") {
+            url.searchParams.append(`${fullKey}[${indexKey}]`, item);
+          } else if (typeof item === "object" && item !== null) {
+            Object.entries(item).forEach(([key, val]) => {
+              const strVal = val as string;
+              url.searchParams.append(
+                `${fullKey}[${indexKey}][${key}]`,
+                strVal,
+              );
+            });
+          }
         });
       } else if (Array.isArray(value)) {
         // Send array values as individual values instead of a comma separated list
--- test/resources/workflows.test.ts
+++ test/resources/workflows.test.ts
@@ -17,6 +17,18 @@ const restHandlers = [
       },
     });
   }),
+  http.get("http://api.knock.test/v1/users/:userID/schedules", () => {
+    return HttpResponse.json({
+      entries: [],
+      page_info: {
+        __typename: "PageInfo",
+        after: null,
+        before: null,
+        page_size: 50,
+        total_count: 0,
+      },
+    });
+  }),
 ];
 
 const server = setupServer(...restHandlers);
@@ -45,6 +57,24 @@ describe("schedules", () => {
     // 'http://api.knock.test/v1/schedules?recipients[]=1&recipients[]=2&recipients[]=3&workflow=test-workflow'
     expect(url).toBe(
       "http://api.knock.test/v1/schedules?recipients%5B%5D=1&recipients%5B%5D=2&recipients%5B%5D=3&workflow=test-workflow",
+    );
+  });
+
+  test("it can list schedules for a given user", async () => {
+    const params: ListSchedulesProps = {
+      recipients: [
+        "user_id",
+        { collection: "object_collection", id: "object_id" },
+      ],
+    };
+    const { url } = await knock.get("/v1/users/user_id/schedules", {
+      ...params,
+    });
+
+    // Formats recipients list as a URL-encoded array
+    // 'http://api.knock.test/v1/users/user_id/schedules?recipients[0]=user_id&recipients[1][collection]=object_collection&recipients[1][id]=object_id'
+    expect(url).toBe(
+      "http://api.knock.test/v1/users/user_id/schedules?recipients%5B0%5D=user_id&recipients%5B1%5D%5Bcollection%5D=object_collection&recipients%5B1%5D%5Bid%5D=object_id",
     );
   });
 });
