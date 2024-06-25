316992de28071d5098774147c89b192aafe99d9f
Chris Bell <cjbell1988@gmail.com>
2024-06-25 10:58:41 -0400

    fix: ensure common/* is exported

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from "./resources/users/interfaces";
 export * from "./resources/preferences/interfaces";
 export * from "./resources/slack/interfaces";
 export * from "./common/interfaces";
+export * from "./common/userTokens";
+export * from "./common/exceptions";
 
 export { Knock } from "./knock";
